f2bed0af430cde1ef49393135c856048a1c6601a
marinagaisina <farigatovna@hotmail.com>
2020-07-31 21:28:52 -0400

    Update mover.js

--- mover.js
+++ mover.js
@@ -1,17 +1,17 @@
 let mover = {
     /**
-     * Receives and sends the direction required by a user
-     * @returns {int} returns the direction required by a user
+     * Receives and sends the direction required by the user
+     * @returns {int} returns the direction required by the user
      */
     getDirection() {
-        const availableDirections = [2, 4, 6, 8];
+        const availableDirections = [1, 2, 3, 4, 6, 7, 8, 9];
         while (true) {
-            let direction = parseInt(prompt('Enter a number (2, 4, 6 or 8) to move, press Cancel for exit the game.'));
+            let direction = parseInt(prompt('Enter a number (1, 2, 3, 4, 6, 7, 8 or 9) to move, press Cancel for exit the game.'));
             if (isNaN(direction)) {
                 return null;
             }
             if (!availableDirections.includes(direction)) {
-                alert('To move enter one of the listed numbers 2, 4, 6 or 8');
+                alert('To move enter one of the listed numbers: 1, 2, 3, 4, 6, 7, 8 or 9');
                 continue;
             }
             return direction;
@@ -30,7 +30,15 @@ let mover = {
         };
         // After we have gotten a direction from a user, updating the current coordinates of the player.
         switch (direction) {
+            case 1:
+                nextPosition.x--;
+                nextPosition.y++;
+                break;
             case 2:
+                nextPosition.y++;
+                break;
+            case 3:
+                nextPosition.x++;
                 nextPosition.y++;
                 break;
             case 4:
@@ -39,9 +47,17 @@ let mover = {
             case 6:
                 nextPosition.x++;
                 break;
+            case 7:
+                nextPosition.x--;
+                nextPosition.y--;
+                break;
             case 8:
                 nextPosition.y--;
                 break;
+            case 9:
+                nextPosition.x++;
+                nextPosition.y--;
+            break;
         }
         if (nextPosition.x < 0 || nextPosition.y < 0 || nextPosition.x >= config.colsCount || nextPosition.y >= config.rowsCount) {
             return player;
